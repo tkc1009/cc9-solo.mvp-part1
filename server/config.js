c75d8dc99eec8584f56924a12821c863b7404130
tkc1009 <takashi.hosoi@live.jp>
2019-08-15 15:31:38 +0900

    add dotenv, .env and settings

--- server/config.js
+++ server/config.js
@@ -2,8 +2,7 @@ module.exports = {
   // database connection configs
   db: {
     client: "pg",
-    connection: process.env.DB_URL || 
-    "postgres://ocfnyejsdqnizf:5a23623c07b9842f0e72d200b080864944a13b5d1f574f7aaa8e75566448c5ae@ec2-174-129-227-128.compute-1.amazonaws.com:5432/d46bv4t0tfbn32?ssl=true&amp;sslfactory=org.postgresql.ssl.NonValidatingFactory",
+    connection: process.env.DB_URL,
   },
 
   // GCP connection configs
